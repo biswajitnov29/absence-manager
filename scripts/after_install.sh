4867dc577751a2de65799ca010753552d1ff0e2a
Biswajit ganguly <biswajit29nov@gmail.com>
2022-09-16 23:56:09 +0800

    AWS deploy config change

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/server
-yarn config set ignore-engines true
-yarn install
-yarn add --save react react-dom react-scripts react-particles-js
-yarn global add pm2
+npm install
+npm install --save react react-dom react-scripts react-particles-js
+npm install -g pm2
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/server
 sudo rm -r build/
-yarn build
+npm run build
 pm2 start server.js
