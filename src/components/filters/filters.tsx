ef6531f35c390f0bf69921091d138a2d44b3db3c
Biswajit ganguly <biswajit29nov@gmail.com>
2022-09-16 17:23:43 +0800

    Adjust packages and remove datepicker

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import ReactDatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import "./filters.css";
 
 type OnChangeProps = {
