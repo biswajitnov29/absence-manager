d97996ef1e4232cf80041dfadeb7dd7eed82d612
Biswajit ganguly <biswajit29nov@gmail.com>
2022-09-19 02:44:32 +0800

    Change file extension

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -60,3 +60,20 @@ export const filterData = (filterCriteria: any, rawList:any[]) => {
   }
   return filterResult;
 };
+
+
+export const IcalFormat = {
+  start: [2019, 12, 3, 6, 30],
+  title: '',
+  description: '',
+  url: '',
+  startOutputType: 'local',
+  duration: { hours: 0, minutes: 30 },
+  location: '',
+  endOutputType: 'local',
+  categories: ['Category A'],
+  status: 'CONFIRMED',
+  busyStatus: "BUSY",
+  organizer: { name: 'Admin' },
+  attendees: []
+};
