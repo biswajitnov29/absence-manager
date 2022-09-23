4a148382e4a2172ab358fa00be91df5bb9f39be0
Biswajit ganguly <biswajit29nov@gmail.com>
2022-09-23 14:53:04 +0800

    Initiate amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "absencemanager": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
